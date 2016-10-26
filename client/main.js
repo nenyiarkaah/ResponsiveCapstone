6d82d21f11b5a4ef2e23088f5cf78247e975c3d8
narkaah <nenyi.arkaah@gmail.com>
2016-10-26 21:51:55 +0100

    working station highlighting

--- client/main.js
+++ client/main.js
@@ -14,23 +14,95 @@ Template.mapSVG.onRendered(() => {
         //find the internal dimensions of SVG to find the ratio for transformation
         //which will fit the map the screen
         var transformRatio = calculateTransformRatio($("svg").parent().width(),
-                        $("svg").parent().height(), svg.select("svg").attr("width"),
-                        svg.select("svg").attr("height"));
-        console.log(transformRatio);
+            $("svg").parent().height(), svg.select("svg").attr("width"),
+            svg.select("svg").attr("height"));
+        // console.log(transformRatio);
         svg.select("svg")
-        //responsive SVG needs these 2 attributes and no width and height attr
-        .attr("preserveAspectRatio", "xMinYMin meet")
-        //class to make it responsive
-        .classed("svg-content-responsive", true)
-        //container class to make it responsive
-        .classed("svg-container", true)
-        .classed("svg-content", true)
-        .attr("transform", "scale(" + transformRatio + ")");
+            //responsive SVG needs these 2 attributes and no width and height attr
+            .attr("preserveAspectRatio", "xMinYMin meet")
+            //class to make it responsive
+            .classed("svg-content-responsive", true)
+            //container class to make it responsive
+            .classed("svg-container", true)
+            .classed("svg-content", true)
+            .attr("transform", "scale(" + transformRatio + ")");
+
+        //add click functionality
+        stops = svg.selectAll("use");
+        stopNames = svg.selectAll("text");
+        intersection = svg.selectAll("intersection");
+
+        stops
+        .on("mouseover", function(){
+            if(isStation(this.href.baseVal)){
+            // console.log(this.id);
+            d3.select(this).style("fill", "#FFA500");
+        }
+        })
+
+        .on("mouseout", function(){
+            if(isStation(this.href.baseVal)){
+            // console.log(this.id);
+            d3.select(this).style("fill", "#ffffff");
+        }
+        })
+
+        .on("click", function(){
+            if(isStation(this.href.baseVal)){
+            console.log(this.id);
+            d3.select(this).style("fill", "808080");
+        }
+        });
+
+        stopNames
+        .on("mouseover", function(){
+
+            console.log(this.textContent);
+            d3.select(this).style("fill", "#FFA500");
+        })
+
+        .on("mouseout", function(){
+            // console.log(this.id);
+            d3.select(this).style("fill", "black");
+        });
+        //
+        // .on("click", function(){
+        //     if(isStation(this.href.baseVal))
+        //     console.log(this.id);
+        //     d3.select(this).style("fill", "808080");
+        // });
+        // .on("mouseout", function(){d3.select(this).style("fill", "808080");});
+
+        intersection
+        .on("mouseover", function(){
+            console.log("hit intersection");
+            d3.select(this).style("fill", "#FFA500");
+        })
+
+        .on("mouseout", function(){
+            d3.select(this).style("fill", "#ffffff");
+        });
+        //
+        // .on("click", function(){
+        //     if(isStation(this.href.baseVal))
+        //     console.log(this.id);
+        //     d3.select(this).style("fill", "808080");
+        // });
+        // .on("mouseout", function(){d3.select(this).style("fill", "808080");});
+        // console.log(stops);
+        console.log(intersection);
     });
 
+    function isStation(type) {
+        if(type.replace("#", "") === "intersection") {
+            return true;
+        }
+        return false;
+    }
+
     function calculateTransformRatio(width, height, svgWidth, svgHeight) {
-        var widthRatio = width/svgWidth;
-        var heightRatio = height/svgHeight;
+        var widthRatio = width / svgWidth;
+        var heightRatio = height / svgHeight;
         var ratio = 1;
         if (heightRatio <= widthRatio) {
             ratio = heightRatio;
